32907ffe691076cf2188db6cbd62dd2398c99beb
alexkaasik <aleks.kaasik@outlook.com>
2025-11-19 19:40:03 +0200

    init the project

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+
+app.use(fileUpload());
+
+app.post("/sign-ssl", (req, res) => {
+    const CertificateSigningRequest = req.files?.CertificateSigningRequest;
+    const DomainName = req.body?.DomainName;
+
+    let errorMessages = [];
+
+    if (!DomainName) {
+        errorMessages.push("The name wasn't added")
+    }
+        
+    if (!CertificateSigningRequest) {
+        errorMessages.push("The file wasn't added")
+    }   
+
+    if (errorMessages.length > 0 ) {
+        return res.status(400).json({ "error": errorMessages });
+    }
+    
+    fs.writeFileSync(`reqs/${DomainName}.req`, CertificateSigningRequest.data);
+
+    try {
+        execSync(`openssl req -in reqs/${DomainName}.req -noout`);
+    } catch (error) {
+        res.status(400).send({ "errr": "Invalid cert" });    
+    }
+
+    execSync(
+        `openssl x509 -req -sha256 -days 365 \
+         -in reqs/${DomainName}.req \
+         -CA ca/ca.pem -CAkey ca/ca-key.pem \
+         -out issued/${DomainName}.pem \
+         -CAcreateserial \
+         -passin pass:${process.env.CA_root_password} \
+         -copy_extensions copy`,
+        { stdio: "inherit" }
+      );
+
+    return res.status(200).sendFile(
+        path.join(path.resolve(), `./issued/${DomainName}.pem`)
+    );
+});
+
+app.listen(3000, () => console.log("SSL Signer API running on port 3000"));
